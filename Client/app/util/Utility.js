d5a03d6a8ee696b970f7d739efe97039cd3cbc52
Luis E. Araujo B <luiseduardoab97@users.noreply.github.com>
2019-02-23 15:13:28 -0400

    Rename project, and refactor code.

--- Client/app/util/Utility.js
+++ Client/app/util/Utility.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Utility extends Component {
     
     state = {
-        url:'http://192.168.43.115:8084/TowerTrivia/'
+        
+        //Modify the URL, and place the server's
+        url:'http://192.168.43.115:8084/MinimalTrivia/'
     }
 
     getUrl(endpoint){
